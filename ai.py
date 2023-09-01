c893fec3bd1be813c5376b598e7b8590f79febb6
10dan <dan.anderson174@gmail.com>
2023-09-01 14:58:10 +0100

    ok its doing somthing..

--- ai.py
+++ ai.py
@@ -2,6 +2,11 @@ import os
 import json
 from PIL import Image
 import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import Model, Input
+from tensorflow.keras.layers import Dense, Reshape, Conv2DTranspose, Conv2D
+import tensorflow as tf
+
 
 def crop_center(image, crop_width=32, crop_height=32):
     width, height = image.size
@@ -11,17 +16,20 @@ def crop_center(image, crop_width=32, crop_height=32):
     bottom = (height + crop_height) / 2
     return image.crop((left, top, right, bottom))
 
+
 def prepare_data():
     recipe_vectors = []
     glaze_images = []
 
-    for folder in os.listdir('recipe_images'):
-        with open(f'recipe_images/{folder}/recipe.json', 'r') as f:
+    for folder in os.listdir("recipe_images"):
+        with open(f"recipe_images/{folder}/recipe.json", "r") as f:
             recipe = json.load(f)
-        
-        for image_file in os.listdir(f'recipe_images/{folder}'):
-            if image_file.endswith('.jpg'):
-                image = Image.open(f'recipe_images/{folder}/{image_file}').convert('RGB')
+
+        for image_file in os.listdir(f"recipe_images/{folder}"):
+            if image_file.endswith(".jpg"):
+                image = Image.open(f"recipe_images/{folder}/{image_file}").convert(
+                    "RGB"
+                )
                 cropped_image = crop_center(image)
                 recipe_vectors.append(recipe)
                 glaze_images.append(np.array(cropped_image))
@@ -31,9 +39,93 @@ def prepare_data():
     glaze_images = np.array(glaze_images)
 
     # Save to disk
-    np.save('pickled_vectors.npy', recipe_vectors)
-    np.save('pickled_images.npy', glaze_images)
+    np.save("pickled_vectors.npy", recipe_vectors)
+    np.save("pickled_images.npy", glaze_images)
+
+
 # prepare_data()
 
-vectors, images = np.load('pickled_vectors.npy'), np.load('pickled_images.npy')
-print(vectors.shape, images.shape)
+
+def create_datasets():
+    vectors, images = np.load("pickled_vectors.npy"), np.load("pickled_images.npy")
+
+    # Normalize images
+    images = images / 255.0
+
+    # Data split
+    training_vectors, test_vectors, training_images, test_images = train_test_split(
+        vectors, images, test_size=0.2, random_state=420
+    )
+
+    (
+        training_vectors,
+        validation_vectors,
+        training_images,
+        validation_images,
+    ) = train_test_split(
+        training_vectors, training_images, test_size=0.2, random_state=420
+    )
+
+    return (
+        training_vectors,
+        validation_vectors,
+        test_vectors,
+        training_images,
+        validation_images,
+        test_images,
+    )
+
+
+def create_nn():
+    input_layer = Input(shape=(291,))
+    x = Dense(128, activation="relu")(input_layer)
+    x = Reshape((4, 4, 8))(x)  # This produces a tensor of shape (None, 4, 4, 8)
+    x = Conv2DTranspose(64, (3, 3), strides=(2, 2), activation="relu", padding="same")(x)  # Output shape: (None, 8, 8, 64)
+    x = Conv2DTranspose(32, (3, 3), strides=(2, 2), activation="relu", padding="same")(x)  # Output shape: (None, 16, 16, 32)
+    x = Conv2DTranspose(32, (3, 3), strides=(2, 2), activation="relu", padding="same")(x)  # Output shape: (None, 32, 32, 32)
+    x = Conv2D(3, (3, 3), activation="sigmoid", padding="same")(x)  # Output shape: (None, 32, 32, 3)
+
+    model = Model(inputs=input_layer, outputs=x)
+
+    # Compile the model
+    model.compile(optimizer="adam", loss="mse")
+
+    # Summary to show the architecture
+    model.summary()
+
+    return model
+
+
+def train_and_save_model(
+    model, training_vectors, validation_vectors, training_images, validation_images
+):
+    # Train the model
+    history = model.fit(
+        training_vectors,
+        training_images,
+        validation_data=(validation_vectors, validation_images),
+        epochs=30,  # You can change the number of epochs
+        batch_size=1,  # You can change the batch size
+    )
+
+    # Save the trained model
+    model.save("trained_model.keras")
+
+    return history
+
+print(tf.sysconfig.get_build_info() )
+# Prepare your data and neural network
+(
+    training_vectors,
+    validation_vectors,
+    test_vectors,
+    training_images,
+    validation_images,
+    test_images,
+) = create_datasets()
+model = create_nn()
+
+# Train and save the model
+train_and_save_model(
+    model, training_vectors, validation_vectors, training_images, validation_images
+)
---
+++ webserver.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify, send_file
+from tensorflow.keras.models import load_model
+import numpy as np
+import PIL.Image
+
+app = Flask(__name__)
+
+# Load your trained model
+model = load_model('trained_model.keras')
+
+@app.route('/')
+def index():
+    return open("index.html").read()
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    recipe_vector = np.array([request.json['recipe']])
+    predicted_image = model.predict(recipe_vector)[0]
+    
+    # Convert to PIL image and save
+    predicted_image = (predicted_image * 255).astype('uint8')
+    img = PIL.Image.fromarray(predicted_image, 'RGB')
+    img.save("/tmp/predicted_image.jpg")
+    
+    return send_file("/tmp/predicted_image.jpg", mimetype='image/jpg')
+
+if __name__ == '__main__':
+    app.run(debug=True)
